3dc12965f3200fc0b112b9021b400033369c829b
sergey96 <altronxr@gmail.com>
2023-07-03 21:42:28 +0300

    create new project

---
+++ Main.php
@@ -0,0 +1,524 @@
+<!DOCTYPE html>
+<html lang="en">
+<head> 
+    <meta charset="UTF-8">
+    <link rel="shortcut icon" href="media/sign_eng.ico" type="image/x-icon"> 
+    <meta name="viewport" content="width=device-width"> 
+    <link rel="stylesheet" href="Style.css">
+    <link rel="stylesheet" href="Adaptive.css">
+    <title>ABC-school</title>
+</head>
+<body>
+<?php require('../Site_7/return.php');?>
+
+<img class="phone_site" src="media/phone.png" alt="">
+
+<aside id="aside_menu">           
+
+<nav class="block_aside_menu">
+    <div class="aside_menu_content">
+    <div class="head_aside_menu">
+        <p>English-School</p>
+        <img class="btn_nav_close" src="media/wClose.png" alt="">
+    </div>
+    <ul class="asise_list">
+        <a href=""><li>Цены</li></a>
+        <a href=""><li>Программа обучения</li></a>
+        <a href=""><li>Наши учителя</li></a>
+        <a href=""><li>Книги</li></a>
+        <a href=""><li>Про школу</li></a>
+    </ul>
+    <div class="bl_aside_soc">
+        <div class="bl_aside_soc_text">
+            <div class="">
+                <p>Есть вопросы? Звоните!</p>
+                <p>0-800-505-33-60</p>
+                <p>0972286444</p>
+            </div><br>
+            <div class="">
+                <img class="img_ic img_ic__loc" src="media/local.png" alt="">
+                <p>Киев, <br>ул. Булаховского 30а,<br> офис 113</p>
+            </div>
+
+        </div>
+    
+    <div class="bl_aside_soc_img">
+        <img src="media/card1.png" alt="">
+        <img src="media/card2.png" alt="">
+        <img src="media/card3.png" alt="">
+    </div>
+    
+    </div>
+    </div>
+</nav>
+
+</aside>
+
+
+<!--------------------------------------------------------------->
+<div class="popUP_signIN">  
+    <div class="signIN_form">
+        <div class="signIN_form_head">
+            <p>Увійти у <span name="inroom">English-School</span> </p>
+            <img class="btnClose_popUP_signIN" src="media/wClose.png" alt="">
+        </div>
+
+        <form  class="signIN_form_body" action="" method="post">
+            <input type="text" name="" id="" placeholder="Логін" ><br/>
+            <input type="password" name="" id="" placeholder="Пароль"><br/><br/>
+            <p>Забули пароль?</p><br/>
+            <input class="btnEng btnEng__form" type="submit" name="" value="Увійти"><br/><br/>
+            <p>Створити обліковий запис</p>
+        </form>
+    </div>
+</div>
+<!--------------------------------------------------------------->
+<!--------------------------------------------------------------->
+
+<div class="popUP_numberCall_block">
+    <div class="popUP_numberCall_block_head">
+        <p class="timerSec">Залишіть номер і ми передзвонимо</p>
+        <img class="img_ic btnClose_popUP_numberCall" src="media/wClose.png" alt="">
+    </div>
+
+    <div class="popUP_numberCall_block_body">
+        <input type="text" placeholder="+380ХХХХХХХХХХ">
+        <button class="btnEng btnEng__call" type="button">Чекати на дзвінок</button>
+    </div>
+</div>
+
+<!--------------------------------------------------------------->
+
+<div class="Wrapper">
+       
+<header id="Head_eng">
+    <div class="menu_head"><img class="img_ic img_ic__menu" src="media/menu.png" alt=""></div>
+    <div class="contacts_head"><img class="img_ic img_ic__loc" src="media/local.png" alt=""><p>Київ, <br>ул. Булаховского 30а,<br> офіс 113</p></div>
+    <div class="logo_head"><p> English-School </p></div>
+    <div class="contacts2_head"><p>Є питання? Дзвоніть!</p><p>0-800-505-33-60</p><p>0972286444</p></div>
+    <div class="signIn_head"> <button type="button" class="btn_sign">Увійти</button></div>
+</header>
+<header id="Head_eng_Scroll">
+    <div class="menu_head"><img class="img_ic img_ic__menu" src="media/menu.png" alt=""></div>
+    <div class="logo_head"><p> English-School </p></div>
+    <div class="signIn_head"> <button type="button" class="btn_sign">Увійти</button></div>
+</header>
+<!--------------------------------------------------------------------------------------->
+
+
+
+
+
+<main id="Main_eng">
+    <img class="main_photo_fon" src="media/fonMain.jpg" alt="">
+    <img class="main_photo_fonDOP" src="media/fonMainDOP.png" alt="">
+
+
+    
+    <div class="main_wrap_content flx_row_ar">
+        <div class="main_block_text">
+            <div class="text_content">
+                <h1> Welcome to English-School </h1>
+                <p>
+               Ми вважаємо, що вивчати англійську мову потрібно в
+                    <span name="r">цікавою</span> та невимушеною манерою.
+                    Дітям у нас подобається, бо навчання
+                    проходить <span name="r">ефективно</span>, весело та комфортно. <br>
+                    <span name="r">Із задоволенням</span> запрошуємо вас і ваших дітей зануритися<br>
+                     у світ англійської мови у нашій школі.
+                </p>
+            </div>
+        </div>
+        <div class="main_block_form">
+            <form class="block_form" action="" method="post">
+           <p> Записуйтесь на <span name="r">безкоштовний</span> урок </p>
+                <input type="text" name="" id="" placeholder="Ім'я дитини"><br>
+                <input type="text" name="" id="" placeholder="Вік"><br>
+                <input type="password" name="" id="" placeholder="+380ХХХХХХХХХ"><br>
+                <input class="btnEng btnEng__form" type="submit" name="" value="Записатися">
+                <img class="img_ben" src="media/ben.png" alt="">
+            </form>
+        </div>
+    </div>
+</main>
+<!--------------------------------------------------------------------------------------->
+
+<section class="Block_1 flx_col_ar">
+<h2> Переваги нашої школи </h2>
+<div class="Block_1_Content">
+
+    <div class="block_advant">
+        <div class="advant_numb">1</div>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit maxime iusto 
+            perspiciatis, ullam 
+            excepturi nobis! Ratione dolorem, reprehenderit perspiciatis porro, 
+            vel animi ea harum rerum iure quo impedit facere asperiores?
+        </p>
+    </div>
+
+    <div class="block_advant">
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit maxime iusto 
+            perspiciatis, ullam 
+            excepturi nobis! Ratione dolorem, reprehenderit perspiciatis porro, 
+            vel animi ea harum rerum iure quo impedit facere asperiores?
+        </p>
+        <div class="advant_numb">2</div>
+    </div>
+
+
+    <div class="block_advant">
+        <div class="advant_numb">3</div>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit maxime iusto 
+            perspiciatis, ullam 
+            excepturi nobis! Ratione dolorem, reprehenderit perspiciatis porro, 
+            vel animi ea harum rerum iure quo impedit facere asperiores?
+        </p>
+    </div>
+
+</div>
+</section>
+
+
+
+
+<!--------------------------------------------------------------------------------------->
+
+<section class="Block_2 flx_col_ar">
+
+    <div class="Block_2_Content">
+        <h3> Акція ! </h3>
+
+        <div class="promo_up">
+            <p>Встигніть зараз записатися, щоб отримати <span>50% знижки</span></p>
+            <div class="timer"></div>
+        </div>
+        <div class="promo_down">
+<!----------------------------->
+            <div class="card_promo">
+                <p>Пакет "Економ"</p>
+               <ul>
+                <li>- Пояснення основ мови</li>
+                <li>- Домашні завдання</li>
+                <li>- Тести</li>
+                <li>- 5 занять</li>
+               </ul>
+               <div class="bl_price">
+                <p>1500₴</p>
+                <button type="button" class="btnEng btnEng__promo"> Записатися </button>
+               </div>
+            </div>
+<!----------------------------->
+<div class="card_promo">
+    <p>Пакет "Стандарт"</p>
+   <ul>
+    <li>- Пояснення основ мови</li>
+    <li>- Пояснення матеріалу</li>
+    <li>- Домашні завдання</li>
+    <li>- Тести</li>
+    <li>- 15 занять</li>
+   </ul>
+   <div class="bl_price">
+    <p name="old_price">3600₴</p>
+    <p name="new_price">1800₴</p>
+    <button type="button" class="btnEng btnEng__promo btnEng__promo_new"> Записатися </button>
+   </div>
+</div>
+<!----------------------------->
+<div class="card_promo">
+    <p>Пакет "Premium"</p>
+   <ul>
+    <li>-Уроки проводитимуть носії мови</li>
+    <li>- Пояснення основ мови</li>
+    <li>- Пояснення матеріалу</li>
+    <li>- Домашні завдання</li>
+    <li>- Тести</li>
+    <li>- 15 занять</li>
+   </ul>
+   <div class="bl_price">
+    <p>4500₴</p>
+    <button type="button" class="btnEng btnEng__promo"> Записатися </button>
+   </div>
+</div>
+<!----------------------------->
+        </div>
+
+    </div>
+
+
+
+</section>
+<!--------------------------------------------------------------------------------------->
+
+
+<section class="Block_3 flx_col_ar">
+    <h4 class="h4_bl_3"> Наші вчителі </h4>
+
+    <div class="teacher_block">
+        <div class="teacher_foto">
+            <img src="media/tech3.jpg" alt="">
+        </div>
+        <div class="teacher_info">
+            <p>пані Оксана</p>
+            <p>
+           <b>Закінчила</b>: Одеський Універсистет;<br>
+               <b>Спеціальність</b>: філолог, психолог;<br><br>
+               <i>"Ми навчимо ваших діток
+                англійської мови"</i>
+            </p>
+
+            <a href="">Сертифікати</a>
+        </div>
+        <div class="teacher_like">
+            <img src="media/star2.png" alt="">
+
+            <div class="like_heart">
+                + <input class="like_inp" type="text" value="0">
+                <img src="media/heart.png" alt="">
+            </div>
+        </div>
+    </div>
+
+    <div class="teacher_block">
+        <div class="teacher_foto">
+            <img src="media/tech2.jpg" alt="">
+        </div>
+        <div class="teacher_info">
+            <p>пані Марина</p>
+            <p>
+            <b>Закінчила</b>: Київський Універсистет;<br>
+               <b>Спеціальність</b>: філолог, психолог;<br><br>
+               <i>"Ми навчимо ваших діток
+                англійської мови"</i>
+            </p>
+
+            <a href="">Сертифікати</a>
+        </div>
+        <div class="teacher_like">
+            <img src="media/star1.png" alt="">
+
+            <div class="like_heart">
+                + <input class="like_inp" type="text" value="17">
+                <img src="media/heart.png" alt="">
+            </div>
+        </div>
+    </div>
+
+
+    <div class="teacher_block">
+        <div class="teacher_foto t_djo">
+            <img src="media/tech1.jpg" alt="">
+        </div>
+        <div class="teacher_info">
+            <p>містер Майк</p>
+            <p>
+              <b>Закінчив</b>: Чиказький Універсистет;<br>
+               А також навчався 2 роки у Києві;<br>
+               <b>Спеціальність</b>: філолог, психолог;<br><br>
+               <i>"Ми навчимо ваших діток
+                англійської мови"</i>
+            </p>
+
+            <a href="">Сертифікати</a>
+        </div>
+        <div class="teacher_like">
+            <img src="media/star3.png" alt="">
+
+            <div class="like_heart">
+                + <input class="like_inp" type="text" value="156">
+                <img src="media/heart.png" alt="">
+            </div>
+        </div>
+    </div>
+
+
+    <div class="teacher_block_DOP">
+        <div class="teacher_block_DOP_morePanel">
+            <h4 class="h4_bl_3_DOP" > Наші вчителі </h4>
+            <button type="button" class="btnEng btnEng_teach">Детальніше</button>
+        </div>
+    </div>
+
+</section>
+
+<!--------------------------------------------------------------------------------------->
+
+<section class="Block_4 flx_col_ar">
+    <h5 class="h5_bl_4" > Як проходять заняття </h5>
+<div class="lessons_block">
+    <div class="lesson l_1">
+        <div class="lesson_fon">
+            <p>
+               Ми розробили веселі випробування, які допомагають засвоювати матеріал.
+            </p>
+        </div>
+    </div>
+    <div class="lesson l_2">
+        <div class="lesson_fon">
+            <p>
+                На уроках збиратимемо пропозиції, як конструктор,
+                Використовуючи граматичні структури.
+            </p>
+        </div>
+    </div>
+    <div class="lesson l_3">
+        <div class="lesson_fon">
+            <p>
+                Ваша дитина постійно впізнаватиме нові англійські слова.
+             </p>
+        </div>
+    </div>
+    <div class="lesson l_4">
+        <iframe width="100%" height="100%" src="https://www.youtube.com/embed/NvW8EnyBUvQ" title="Степени сравнения прилагательных | Английский для начинающих" frameborder="0" 
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    </div>
+</div>
+
+
+<div class="lessons_block_DOP">
+    <div class="lessons_block_DOP_morePanel">
+        <h5 class="h5_bl_4_DOP" > Як проходять заняття </h5>
+        <button type="button" class="btnEng btnEng_lesson">Детальніше</button>
+    </div>
+
+</div>
+
+</section>
+
+<!--------------------------------------------------------------------------------------->
+
+
+
+
+<section class="Block_5 flx_col_ar">
+    <h6> Відгуки батьків </h6>
+
+<div class="block_feedback">
+<!----------------------------------->
+    <div class="feed_1"><img src="media/client1.png" alt=""><p>Ірина, Одесса</p>
+        <p>
+            <b>Відгук №1</b><br>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+            Quos beatae asperiores obcaecati 
+            delectus culpa, quis, eius nihil voluptates, aut pariatur amet 
+            incidunt doloremque voluptatum. Veniam enim tenetur quis in consequuntur?
+        </p>
+    </div>
+<!----------------------------------->
+<div class="feed_1"><img src="media/client3.jpg" alt=""><p>Дмитро, Київ</p>
+    <p>
+        <b>Відгук №2</b><br>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+        Quos beatae asperiores obcaecati 
+        delectus culpa, quis, eius nihil voluptates, aut pariatur amet 
+        incidunt doloremque voluptatum. Veniam enim tenetur quis in consequuntur?
+    </p>
+</div>
+<!----------------------------------->
+<div class="feed_1"><img src="media/client2.png" alt=""><p>Ольга, Вінниця</p>
+    <p>
+        <b>Відгук №3</b><br>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+        Quos beatae asperiores obcaecati 
+        delectus culpa, quis, eius nihil voluptates, aut pariatur amet 
+        incidunt doloremque voluptatum. Veniam enim tenetur quis in consequuntur?
+    </p>
+</div>
+<!----------------------------------->
+    <div class="feed_1"><img src="media/client3.jpg" alt=""><p>Дмитро, Київ</p>
+        <p>
+            <b>Відгук №4</b><br>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+            Quos beatae asperiores obcaecati 
+            delectus culpa, quis, eius nihil voluptates, aut pariatur amet 
+            incidunt doloremque voluptatum. Veniam enim tenetur quis in consequuntur?
+        </p>
+    </div>
+    <div class="feed_1"><img src="media/client1.png" alt=""><p>Ірина, Одесса</p>
+        <p>
+            <b>Відгук №5</b><br>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+            Quos beatae asperiores obcaecati 
+            delectus culpa, quis, eius nihil voluptates, aut pariatur amet 
+            incidunt doloremque voluptatum. Veniam enim tenetur quis in consequuntur?
+        </p>
+    </div>
+    <!----------------------------------->
+        <div class="feed_1"><img src="media/client2.png" alt=""><p>Ольга, Вінниця</p>
+            <p>
+                <b>Відгук №6</b><br>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+                Quos beatae asperiores obcaecati 
+                delectus culpa, quis, eius nihil voluptates, aut pariatur amet 
+                incidunt doloremque voluptatum. Veniam enim tenetur quis in consequuntur?
+            </p>
+        </div>
+    </div>
+
+    <div class="btn_slider_group">
+        <img class="btn_prev" src="media/prev.png" alt="">
+        <img class="btn_next" src="media/next.png" alt="">
+    </div>
+
+
+
+<button type="button" class="btnEng btnEng__feed"> Залишити відгук </button>
+</section>
+
+
+    
+<footer id="Footer_eng">
+    <div class="footer_list footer_contacts">
+        <ul>Контакти
+            <li>0958253667</li>
+            <li>0800-505-36-15</li>
+            <li>Київ, ул. Перемоги 27, 2-поверх, офіс 5</li>
+        </ul>
+<div class="block_footrSoc">
+    <img src="media/card2.png" alt="">
+    <img src="media/card3.png" alt="">
+    <img src="media/card4.png" alt="">
+</div>
+
+        <div class="blockMap">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d30277.8823601496!2d30.386488939165286!3d50.45371898288515!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40d4cc1ea2cba133%3A0xd69cacdd52f42944!2sEmpire%20English%20School!5e0!3m2!1sru!2sua!4v1687201849147!5m2!1sru!2sua" width="350" height="230" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+    </div>
+  
+    
+<div class="footer_list footer_menu">
+    <ul>Меню
+        <li>Акції</li>
+        <li>Ціни</li>
+        <li>Наші вчителі</li>
+        <li>Книги</li>
+        <li>Заходи</li>
+    </ul>
+</div>
+
+<div class="footer_list footer_registr">
+<ul>Особисті дані
+    <li>Вхід до кабінету</li>
+    <li>Реєстрація</li>
+</ul>
+
+<div class="block_pay">
+    <p>Сплатити можна тут</p>
+    <img src="media/privat.png" alt="">
+</div>
+</div>
+
+<div class="footer_downPanel">
+    <p>2012-2022р | "English-ABC" | Усі права захищені</p>
+    <p>Політика конфіденційності </p>
+</div>
+</footer>
+
+</div><!------ END Main -------->
+
+<script src="https://code.jquery.com/jquery-3.6.3.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
+<script src="abc.js"></script>
+</body>
+</html>
